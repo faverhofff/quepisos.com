401c30320a599b1e6203340158bfdde85d6045e6
frank <faverhofff@gmail.com>
2017-02-23 12:40:25 -0500

    Test

--- app/Http/Breadcrumbs/Backend/LogViewer.php
+++ app/Http/Breadcrumbs/Backend/LogViewer.php
@@ -2,21 +2,20 @@
 
 Breadcrumbs::register('admin.log-viewer::dashboard', function ($breadcrumbs) {
     $breadcrumbs->parent('admin.dashboard');
-    //$breadcrumbs->push(trans('menus.backend.log-viewer.main'), url('admin/log-viewer'));
-    $breadcrumbs->push(trans('menus.backend.log-viewer.main'), env('APP_URL').'admin/log-viewer');
+    $breadcrumbs->push(trans('menus.backend.log-viewer.main'), url('admin/log-viewer'));
 });
 
 Breadcrumbs::register('admin.log-viewer::logs.list', function ($breadcrumbs) {
     $breadcrumbs->parent('admin.log-viewer::dashboard');
-    $breadcrumbs->push(trans('menus.backend.log-viewer.logs'), env('APP_URL').('admin/log-viewer/logs'));
+    $breadcrumbs->push(trans('menus.backend.log-viewer.logs'), url('admin/log-viewer/logs'));
 });
 
 Breadcrumbs::register('admin.log-viewer::logs.show', function ($breadcrumbs, $date) {
     $breadcrumbs->parent('admin.log-viewer::logs.list');
-    $breadcrumbs->push($date, env('APP_URL').('admin/log-viewer/'.$date));
+    $breadcrumbs->push($date, url('admin/log-viewer/'.$date));
 });
 
 Breadcrumbs::register('admin.log-viewer::logs.filter', function ($breadcrumbs, $date, $filter) {
     $breadcrumbs->parent('admin.log-viewer::logs.show', $date);
-    $breadcrumbs->push(ucfirst($filter), env('APP_URL').('admin/log-viewer/'.$date.'/'.$filter));
+    $breadcrumbs->push(ucfirst($filter), url('admin/log-viewer/'.$date.'/'.$filter));
 });
